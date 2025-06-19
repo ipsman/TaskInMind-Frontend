cb7fdce10e7f9a9d920c2517b7d9d96bc7bd5265
ipsman <fogkefegaming@gmail.com>
2025-06-19 22:54:13 +0200

    Settings added

--- src/app/basePage.js
+++ src/app/basePage.js
@@ -6,6 +6,7 @@ import Sidenav from "./SideNav/Sidenav";
 import { format, addMonths } from 'date-fns';
 import TopNav from './Top/topNav';
 import DayPlan from './Center/dayPlan';
+import Settings from './Center/settings';
 
 const BasePage = ({ onLogout }) => {
 
@@ -32,6 +33,7 @@ const BasePage = ({ onLogout }) => {
         goToToday={goToToday}
       />
       <DayPlan></DayPlan>
+      <Settings></Settings>
       <div className="flex h-[calc(100%-55px)]">
         <Sidenav onLogout={onLogout}></Sidenav>
         <div className="h-full w-full">
@@ -40,7 +42,11 @@ const BasePage = ({ onLogout }) => {
             setCurrentMonth={setCurrentMonth}
           />
         </div>
-        <div className="dark:bg-[#000000b9] bg-[#ffffffb9] px-[10px] h-full w-[80px]"></div>
+        <div className="dark:bg-[#000000b9] bg-[#ffffffb9] px-[10px] h-full w-[80px]">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z" />
+          </svg>
+        </div>
       </div>
     </div>
   );
